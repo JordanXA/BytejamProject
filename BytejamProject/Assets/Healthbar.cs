4d03b396ec6897128dc347ede80bbe3a9223bb55
Jordan <>
2020-11-09 13:13:00 -0600

    Added health
    nothing happens when you lose doe

---
+++ BytejamProject/Assets/Healthbar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    public float maxWidth = 280;
+    public float height = 45.5f;
+    
+    private void OnGUI() {
+        GlobalVariables global = GameObject.Find("GameManager").GetComponent<GlobalVariables>();
+        GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth*global.health/global.maxHealth,height);
+    }
+}
