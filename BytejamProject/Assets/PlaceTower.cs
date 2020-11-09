39c61014b5dd2f0fe554522f3eaf7ef7893e571e
themorticianer <lucrurup@gmail.com>
2020-11-09 13:42:38 -0600

    basic placement

---
+++ BytejamProject/Assets/PlaceTower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTower : MonoBehaviour
+{
+    public GameObject TowerPrefab;
+    private GameObject Tower;
+    private bool CanPlaceTower()
+    {
+        return Tower == null;
+    }
+
+    void OnMouseUp()
+    {
+
+        if (CanPlaceTower())
+        {
+
+            Tower = (GameObject)
+              Instantiate(TowerPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+}
