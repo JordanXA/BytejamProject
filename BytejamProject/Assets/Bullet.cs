0668f6445e69e65da91dd7564eb31429d32e727f
Jordan <>
2020-11-10 09:13:08 -0600

    Made cannons shoot tanks good
    Good

---
+++ BytejamProject/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float angle;
+    public float speed;
+    public Vector2 velocity;
+    public float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float rad = angle * Mathf.Deg2Rad;
+        velocity = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(velocity.x, velocity.y, 0);
+    }
+
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
---
+++ BytejamProject/Assets/Tower.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    public float angleOffset;
+    public float reloadTime;
+    float timeSinceLastShot;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    Enemy getLastEnemy() {
+        Enemy[] enemyList = GameObject.FindObjectsOfType<Enemy>();
+        return enemyList[enemyList.Length-1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy enemy = getLastEnemy();
+        bool towerShot = false;
+
+        if (enemy != null) { //enemy exists
+            Vector2 enemyPos = enemy.transform.position;
+            Vector2 posDiff = enemyPos -  (Vector2)transform.position;
+            posDiff.Normalize(); //fancy math for angle
+            float angle = Mathf.Atan2(posDiff.y, posDiff.x) * Mathf.Rad2Deg;
+            Quaternion currentRotation = transform.rotation;
+            currentRotation.eulerAngles = new Vector3(0,0,angle+angleOffset);
+            transform.rotation = currentRotation;
+
+            if (timeSinceLastShot > reloadTime) {
+                Shoot(enemy, angle);
+                towerShot = true;
+                timeSinceLastShot = 0;
+            }
+        }
+
+        if (!towerShot) {
+            timeSinceLastShot += Time.deltaTime;
+        }
+
+    }
+
+    void Shoot(Enemy enemy, float angle) {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, new Quaternion());
+        bullet.GetComponent<Bullet>().angle = angle;
+    }
+}
